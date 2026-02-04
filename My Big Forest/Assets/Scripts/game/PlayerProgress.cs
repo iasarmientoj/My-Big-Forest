db80e877ead4bac171d71b7e13c8fd242d1e6c4a
Iván Sarmiento <41652611+iasarmientoj@users.noreply.github.com>
2026-02-04 17:51:00 -0500

    sonido hongo

--- My Big Forest/Assets/Scripts/game/PlayerProgress.cs
+++ My Big Forest/Assets/Scripts/game/PlayerProgress.cs
@@ -5,10 +5,23 @@ public class PlayerProgress : MonoBehaviour
 {
     public int mushroomCount = 0;
     public static event Action<int> OnMushroomCollected;
+    
+    public AudioSource audioSource;
+    public AudioClip collectionSound;
 
     public void AddMushroom()
     {
         mushroomCount++;
+        
+        if (audioSource != null && collectionSound != null)
+        {
+            audioSource.PlayOneShot(collectionSound);
+        }
+        else
+        {
+            Debug.LogWarning("Falta asignar el AudioSource o el AudioClip en el script PlayerProgress del jugador.");
+        }
+
         OnMushroomCollected?.Invoke(mushroomCount);
     }
 }
